fd2e082876435fb9dcf328f632ffa02a2b98598f
Marina Rodrigues <60441273+rodriguesmari@users.noreply.github.com>
2022-03-18 08:49:46 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#funcionalidades
+
+@app.route('/')
+def homepage():
+  return'A API está no ar'
+
+@app.route('/pegarvendas')
+def pegarvendas():
+  tabela = pd.read_csv('advertising.csv')
+  total_vendas = tabela['Vendas'].sum()
+  resposta = {'total_vendas': total_vendas}
+  return jsonify(resposta)
+  
+#rodar api
+app.run(host='0.0.0.0')
